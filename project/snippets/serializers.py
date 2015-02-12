8414e7dac89a01f4abbfdd91d458f1f6e3b2a59a
Aliou BA <alioubafr@gmail.com>
2015-02-12 19:27:15 +0100

    Test with relations nested

--- project/snippets/serializers.py
+++ project/snippets/serializers.py
@@ -3,14 +3,9 @@ from rest_framework import serializers
 from snippets.models import *
 
 
-class TrackSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Track
-        fields = ('order', 'title','album_id')
-
 class AlbumSerializer(serializers.ModelSerializer):
-    tracks = TrackSerializer(source='album_id',many=True, read_only=True)
+    tracks = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Album
-        fields = ('id', 'artist', 'tracks')
+        fields = ('artist', 'tracks')
