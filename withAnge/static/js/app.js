21bd730e8fb1b90fb02a4859bc236ffd217eccc7
Aliou BA <alioubafr@gmail.com>
2015-02-15 23:58:07 +0100

    JS template

--- withAnge/static/js/app.js
+++ withAnge/static/js/app.js
@@ -34,23 +34,11 @@ my_app.controller("creerCompanyCtrl", function($scope, $location, $filter ,$http
 		$scope.showConf = false;		
 	};
 	$scope.toActByGroup = function(allActivities){
-		$scope.groups = []; 
-		$scope.activities = []; 
-		for (var i = 1; i < allActivities.length; i++) {
-			if(allActivities[i]["name"] in $scope.groups){
-				$scope.activities[allActivities[i]["name"]].push(allActivities[i]);
-			}
-			else{
-				$scope.groups.push(allActivities[i]["name"]);
-				$scope.activities[allActivities[i]["name"]].push(allActivities[i]);
-			}
-			return $scope.activities;
-		};
+
 	};
 	$scope.allActivities = [];
         $http.get('/prestaviticoles/api/activities/1234567891/?format=json')
           .success(function(out_data) {
           	$scope.allActivities = out_data ;
-          	console.log($scope.toActByGroup($scope.allActivities)); 
         });
 });
