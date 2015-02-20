1665ff134d9743a1336736d38778023b7bc7b917
Aliou BA <alioubafr@gmail.com>
2015-02-20 16:04:08 +0100

    Directives

--- withAnge/static/js/MesDirectives.js
+++ withAnge/static/js/MesDirectives.js
@@ -2,32 +2,32 @@ angular.module("MesDirectives", [])
 .directive("contenuHomeDevis", function() {
 	return {
 		restrict: "E",
-        templateUrl: '/static/directivesHTML/homeDevis.html',
-        scope: {
-        	entr: "=",        	
-        	conf: "=",
-        	selectparam: "&"
-        }
+                templateUrl: '/static/directivesHTML/homeDevis.html',
+                scope: {
+                	entr: "=",        	
+                	conf: "=",
+                	selectparam: "&"
+                }
 	}
 })
 .directive("contenuParamsPlants", function() {
 	return {
 		restrict: "E",
-        templateUrl: '/static/directivesHTML/paramsPlantDevis.html',
-        scope: {
-        	conf: "=",
-        	selecthome: "&",
-        	selectplant: "&"
-        }
+                templateUrl: '/static/directivesHTML/paramsPlantDevis.html',
+                scope: {
+                	conf: "=",
+                	selecthome: "&",
+                	selectplant: "&"
+                }
 	}
 })
 .directive("contenuPlants", function() {
 	return {
 		restrict: "E",
-        templateUrl: '/static/directivesHTML/PlantsDevis.html',
-        scope: {
-        	activities: "=",
-        	selectparam: "&"
-        }
+                templateUrl: '/static/directivesHTML/PlantsDevis.html',
+                scope: {
+                	activities: "=",
+                	selectparam: "&"
+                }
 	}
 })
