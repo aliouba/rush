8700302cb97b8c7d57d4fbfbb380a1284549603d
Aliou BA <alioubafr@gmail.com>
2015-04-09 22:22:12 +0200

    Views pour le POST create benefits

--- devis/presta_viticoles/views.py
+++ devis/presta_viticoles/views.py
@@ -23,9 +23,7 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 
 def make_estimate(request,siret):
-    print "func"
     if request.method == 'POST':
-        print "post"
         benefitsSelected = {}
         estimate = {}
         """
@@ -37,7 +35,6 @@ def make_estimate(request,siret):
         Userparaams = data['allparams']
         #Check USER
         user_params_checked = check_post_new_estimate(request,Userparaams)
-        print user_params_checked
 
 
         company = Company.objects.filter(siret=siret)[0]
@@ -141,12 +138,10 @@ class EstimatesCustomerList(APIView):
         return Response(serializer.data)
 
 def create_estimate(allparams,allbenefits):
-    print "okk"
     try:
-        newcustomer = Customer.objects.filter(user=allparams['customer']).get()
+        newcustomer = Customer.objects.get(user=allparams['customer'])
     except Exception, e:
         newcustomer = Customer(mail=allparams['mail'],user=allparams['customer'])
-        print "ll"
         newcustomer.save()
     if allparams['plant_superficie']=="sup":
         newestimate = Estimate(nb=allparams['nb'],customer=newcustomer,plant_superficie=allparams['plant_superficie'],type_guyot=allparams['type_guyot'],distance_entre_ceps=allparams['distance_entre_ceps'],largeur_entre_rangs=allparams['largeur_entre_rangs'],surface=allparams['surface'])
@@ -154,16 +149,25 @@ def create_estimate(allparams,allbenefits):
     else:
         newestimate = Estimate(nb=allparams['nb'],customer=newcustomer,plant_superficie=allparams['plant_superficie'],type_guyot=allparams['type_guyot'])
         newestimate.save()
+    h=0.0
+    price_ttc=0.0
     for key  in allbenefits:
-        newbenefit = Benefit(unit_price=allbenefits[key]['unitPrice'],tax=allbenefits[key]['tax'],activity_id=allbenefits[key]['activity'],estimate=newestimate)
+        one_price_ttc=float(allbenefits[key]['unitPrice']) * allparams['nb']
+        price_ttc =price_ttc + one_price_ttc
+        one_h = float(allbenefits[key]['unitPrice']) * float(allbenefits[key]['tax']) *allparams['nb'] /100 
+        h = h + one_h      
+        newbenefit = Benefit(unit_price=allbenefits[key]['unitPrice'],tax=allbenefits[key]['tax'],price_with_tax=one_price_ttc,price_without_tax=one_price_ttc-one_h,activity_id=allbenefits[key]['activity'],estimate=newestimate)
         newbenefit.save()
+    newestimate.price_with_tax = price_ttc
+    price_ht = price_ttc - h
+    newestimate.price_without_tax = price_ht
+    newestimate.save()
 def check_post_new_estimate(request,allparams):
     estimate = {}
     if allparams['parPlant']:
         estimate['nb'] = int(allparams['nombrePlants'])
         estimate['type_guyot'] = allparams['optionsguyot']
         estimate['plant_superficie'] = 'plt'
-        print "par plant"
     elif allparams['parSuperficie']:
         estimate['type_guyot'] = allparams['optionsguyot']
         estimate['nb'] = int(allparams['nombrePlants'])
@@ -171,22 +175,17 @@ def check_post_new_estimate(request,allparams):
         estimate['surface'] = float(allparams['optionssuperficie'])
         estimate['distance_entre_ceps'] = float(allparams['optionsdistceps'])
         estimate['largeur_entre_rangs'] = float(allparams['optionsdistrangs'])
-        print "ok"
     if request.user.is_authenticated():
         estimate['customer'] = request.user
         estimate['mail'] = request.user.email
-        print "ok1"
     else:
-        print allparams['mail']
         try:
-            user = User.objects.filter(email=allparams['mail']).get()
+            user = User.objects.get(email=allparams['mail'])
             estimate['customer'] = user
-            estimate['mail'] = allparams['mail']
-            print "yes"
+            estimate['mail'] = user.email
         except Exception, e:
             new_password = User.objects.make_random_password()
             user = User.objects.create_user(allparams['mail'],allparams['mail'],new_password)
             estimate['customer'] = user 
             estimate['mail'] = allparams['mail']
-            print "ok1"
     return estimate
